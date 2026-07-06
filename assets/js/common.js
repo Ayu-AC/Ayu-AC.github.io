dead04ff39177a7b703dba8cc5fa0f74d095a69b
99654251@qq.com <zengyuliu@smu.edu.sg>
2026-07-06 22:12:54 +0800

    Initial commit: personal academic homepage

---
+++ assets/js/common.js
@@ -0,0 +1,27 @@
+﻿// Lazy loading for images
+$(function() {
+    $('.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+});
+
+// Smooth scroll for the year nav on publications page
+$(document).ready(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 90
+                }, 500);
+                return false;
+            }
+        }
+    });
+});
